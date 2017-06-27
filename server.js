5d3c41aebd850e339440c2a193619f940c7b610a
swetapatnaik88 <swetapatnaik88@users.noreply.github.com>
2017-06-28 00:30:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,85 +14,31 @@ app.get('/ping', function(req, res) {
     res.send('success\n\n' + new Date());
 });
 
-app.post('/api/CasApiService/GetView', function(req, res) {
-    console.log('POST:' + '/api/CasApiService/GetView');
-    const customers = JSON.parse(fs.readFileSync('responses/BpaAccountOverview/GetView.json'));
-    //console.log('Response: \n' + JSON.stringify(customers));
-    res.send(customers);
+app.post('/sitecore/api/enrollment/account/register', function(req, res) {
+    console.log('POST:' + '/sitecore/api/enrollment/account/register');
+    const register = JSON.parse(fs.readFileSync('responses/register.json'));
+    res.send(register);
 });
 
-app.post('/api/CasApiService/SaveBo', function(req, res) {
-    console.log('POST:' + '/api/CasApiService/SaveBo');
-    console.log('I am not implemented, returning empty response');
-    res.send({});
+app.get('/sitecore/api/enrollment/basket/get', function(req, res) {
+    console.log('GET:' + '/sitecore/api/enrollment/basket/get');
+    const selection = JSON.parse(fs.readFileSync('responses/selection.json'));
+    console.log('Response: \n' + JSON.stringify(selection));
+    res.send(selection);
 });
 
-app.post('/api/CasApiService/CallMethod', function(req, res) {
-    console.log('POST:' + '/api/CasApiService/CallMethod');
-    console.log('I am not implemented, returning empty response');
-    res.send({});
+app.get('/sitecore/api/enrollment/information/get', function(req, res) {
+    console.log('GET:' + '/sitecore/api/enrollment/information/get');
+    const information = JSON.parse(fs.readFileSync('responses/information.json'));
+    console.log('Response: \n' + JSON.stringify(information));
+    res.send(information);
 });
 
-app.post('/api/CasApiService/GetAppMenu', function(req, res) {
-    console.log('POST:' + '/api/CasApiService/GetAppMenu');
-    const menu = JSON.parse(fs.readFileSync('responses/GetAppMenu.json'));
-    //console.log('Response: \n' + JSON.stringify(customers));
-    res.send(menu);
-});
-
-app.post('/api/CasApiService/GetToggles', function(req, res) {
-    console.log('POST:' + '/api/CasApiService/GetToggles');
-    const customers = JSON.parse(fs.readFileSync('responses/BpaAccountOverview/GetToggle.json'));
-    console.log('Response: \n' + JSON.stringify(customers));
-    res.send(customers);
-});
-
-app.post('/api/CasApiService/GetText', function(req, res) {
-    console.log('POST:' + '/api/CasApiService/GetText');
-    const customers = JSON.parse(fs.readFileSync('responses/BpaAccountOverview/GetText.json'));
-    console.log('Response: \n' + JSON.stringify(customers));
-    res.send(customers);
-});
-
-app.post('/api/CasApiService/GetRequest', function(req, res) {
-    console.log('POST:' + '/api/CasApiService/GetRequest');
-    const customers = JSON.parse(fs.readFileSync('responses/BpaAccountOverview/GetRequest.json'));
-    //console.log('Response: \n' + JSON.stringify(customers));
-    res.send(customers);
-});
-
-app.post('/api/CasApiService/GetProfiles', function(req, res) {
-    console.log('POST:' + '/api/CasApiService/GetProfiles');
-    const profiles = JSON.parse(fs.readFileSync('responses/BpaAccountOverview/GetProfiles.json'));
-    res.send(profiles);
-});
-
-app.get('/api/CasApiService/Login', function(req, res) {
-    console.log('GET:' + '/api/CasApiService/Login');
-    const customers = JSON.parse(fs.readFileSync('responses/login.json'));
-    console.log('Response: \n' + JSON.stringify(customers));
-    res.send(customers);
-});
-
-app.get('/api/CasApiService/Logout', function(req, res) {
-    console.log('GET:' + '/api/CasApiService/Logout');
-    //const customers = JSON.parse(fs.readFileSync('responses/logout.json'));
-    //console.log('Response: \n' + JSON.stringify(customers));
-    //res.send(customers);
-});
-
-app.post('/api/CasApiService/GetViewAsToggle', function(req, res) {
-    console.log('POST:' + '/api/CasApiService/GetViewAsToggle');
-    const customers = JSON.parse(fs.readFileSync('responses/BpaAccountOverview/GetViewAsToggle.json'));
-    console.log('Response: \n' + JSON.stringify(customers));
-    res.send(customers);
-});
-
-app.post('/api/CasApiService/GetTotalRowCount', function(req, res) {
-    console.log('POST:' + '/api/CasApiService/GetTotalRowCount');
-    const customers = JSON.parse(fs.readFileSync('responses/BpaAccountOverview/GetTotalRowCount.json'));
-    console.log('Response: \n' + JSON.stringify(customers));
-    res.send(customers);
+app.get('/sitecore/api/enrollment/confirmation/get', function(req, res) {
+    console.log('GET:' + '/sitecore/api/enrollment/confirmation/get');
+    const confirmation = JSON.parse(fs.readFileSync('responses/confirmation.json'));
+    console.log('Response: \n' + JSON.stringify(confirmation));
+    res.send(confirmation);
 });
 
 const server = app.listen(20403, () => {
